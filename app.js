4e92f36daa52c4aae4edb8e270e0f9e82bf4beb7
Ysael Saez <ysael.saez@outlook.com>
2021-08-08 15:30:58 -0400

    add data values and methods to vue object
    also add basic sum counter for seconds

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 const app = Vue.createApp({
   data() {
-    return {};
+    return {
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+      intervalId: null,
+    };
   },
-  methods: {},
+  methods: {
+    startTimer() {
+      let intervalId = setInterval(() => this.seconds++, 1000);
+      this.intervalId = intervalId;
+    },
+    stopTimer() {
+      clearInterval(this.intervalId);
+    },
+  },
 });
 
 app.mount("#timer");
